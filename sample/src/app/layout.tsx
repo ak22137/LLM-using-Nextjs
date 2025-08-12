31b60cfd5a723ee4ff3da611a525cbf1337834f9
Akshat Singh Panwar <akshatyt222@gmail.com>
2025-08-12 22:51:20 +0530

    -

--- sample/src/app/layout.tsx
+++ sample/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
