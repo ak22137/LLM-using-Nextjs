f80102ee8c96a8648c4ae5a89c2eaac1d3508002
Akshat Singh Panwar <akshatyt222@gmail.com>
2025-08-12 22:39:23 +0530

    -

--- sample/src/trpc.ts
+++ sample/src/trpc.ts
@@ -2,11 +2,3 @@ import { createTRPCReact } from '@trpc/react-query';
 import { httpBatchLink } from '@trpc/client';
 
 export const trpc = createTRPCReact();
-
-export const client = trpc.createClient({
-  links: [
-    httpBatchLink({
-      url: '/api/trpc',
-    }),
-  ],
-});
